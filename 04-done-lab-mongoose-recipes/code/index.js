7c6ab2fcb94e1af7639446553faf37a3eba13c1f
sandrabosk <sandraboskovic@hotmail.com>
2019-09-21 16:55:55 -0400

    add comments on solution for mongoose recipes lab

--- 04-done-lab-mongoose-recipes/code/index.js
+++ 04-done-lab-mongoose-recipes/code/index.js
@@ -22,13 +22,19 @@ let newRecipe = {
 };
 
 Recipe
+// we are passing an object (newRecipe) to the .create() method and 
+// under the hood, this method will insert this object as a new document in the database in the "recipes" collection
   .create(newRecipe)
+  // .then() resolves if everything worked fine
   .then(result => console.log(`recipe added: ${result.title}`))
+  // .catch() handles errors
   .catch(err => console.log(err));
 
 // iteration 3
 
 Recipe
+// here we are passing an array of objects to the ".insertMany()" method that literally does what it says:
+// inserts multiple objects into the database as documents of the "recipes" collection
   .insertMany(data)
   .then(result => {
     result.forEach(item => {
@@ -40,6 +46,9 @@ Recipe
 // iteration 4
 
 Recipe
+// under the hood, ".updateOne()" basically works as ".findOneAndUpdate()" method:
+// finds a certain document in the database based on some criteria (in this case based on the "title"), 
+// and sets some of the properties to the new values and saves it in the database
   .updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
   .then(() => console.log(`The recipe is updated`))
   .catch(err => console.log(err));
@@ -47,6 +56,8 @@ Recipe
 // iteration 5
 
 Recipe
+// under the hood, it works as ".findOneAndDelete" method - find doc based on some criteria (in this case title but could be any other property)
+// and removes it from the DB permanently
   .deleteOne({title: 'Carrot Cake'})
   .then(() => console.log(`The recipe is deleted`))
   .catch(err => console.log(err));
@@ -54,6 +65,7 @@ Recipe
 // iteration 6
 
 mongoose.connection
+// alternative way to using ".close()" method is using ".disconnect()". The best practice is to use ".close()" but you will see sometimes the alternative way as well
   .close()
   .then(() => console.log(`connection closed`))
   .catch(err => console.log(`an error while closing database connection has occurred: ${err}`));
