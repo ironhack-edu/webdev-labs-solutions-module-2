cfb1f97d12997ab845dcb882ec24e07854c7a910
sandrabosk <sandraboskovic@hotmail.com>
2019-09-26 17:22:15 -0400

    add comments on solution for ironrooms lab

--- 12-done-lab-ironrooms/routes/auth-routes.js
+++ 12-done-lab-ironrooms/routes/auth-routes.js
@@ -8,30 +8,43 @@ const bcrypt = require('bcryptjs');
 const bcryptSalt = 10;
 
 router.get('/signup', (req, res, next) => {
+  // renders the page with the sign up form from the "views/auth" folder
   res.render('auth/signup');
 })
 
-// action="/register"
+//<form action="/register" method="POST">
+//                |
+//                V
 router.post('/register', (req, res, next) => {
   const userEmail = req.body.email;
   const userPassword = req.body.password;
   const userFullName = req.body.fullName;
 
+  // if any of the following fields are left empty, send warning to users
   if(userEmail == '' || userPassword == '' || userFullName == ''){
-    req.flash('error', 'Please fill all the fields.')
+    req.flash('error', 'Please fill all the fields.');
+
+    // and render the form again
     res.render('auth/signup');
+
+    // we return because we want to to stop execution on this route until user submits the data so we can proceed
     return;
   }
 
+
+  // find user by inputted email
   User.findOne({ email: userEmail })
   .then(foundUser => {
+    // if you find user already saved in the DB with the same email, send them warning
     if(foundUser !==null){
       req.flash('error', 'Sorry, there is already user with the same email!');
-      // here we will redirect to '/login' 
+      // here we will redirect to '/login' since they already have profile (they are in the DB)
       res.redirect('/login');
       return;
     }
 
+
+    // if there's no user with the email they just input, proceed to saving user in the DB
     const salt = bcrypt.genSaltSync(bcryptSalt);
     const hashPassword = bcrypt.hashSync(userPassword, salt);
 
@@ -55,13 +68,15 @@ router.post('/register', (req, res, next) => {
       .catch( err => next(err)); //closing User.create()
   })
   .catch( err => next(err)); // closing User.findOne();
-})
+});
 
 //////////////// LOGIN /////////////////////
 router.get('/login', (req, res, next) => {
+  // renders the page with the login up form from the "views/auth" folder
   res.render('auth/login');
-})
+});
 
+// we use passport.authenticate() method with local strategy to verify user's credentials and let them login
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/private', // <== successfully logged in
   failureRedirect: '/login', // <== login failed so go to '/login' to try again
