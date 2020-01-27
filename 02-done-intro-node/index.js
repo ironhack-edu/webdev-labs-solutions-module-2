3d6dae8e3ddeed77853d8edd98e6746f9479f89c
sandrabosk <sandraboskovic@hotmail.com>
2020-01-27 17:18:29 -0500

    add intro node and ironbeers updated solutions

--- 02-done-intro-node/index.js
+++ 02-done-intro-node/index.js
@@ -3,25 +3,27 @@ class SortedList {
     // we have to use keyword "this" to point out that these are the properties of the SortedList class
     // items should be an array
     this.items = [];
-    // length should be the number of elements in the array
     this.length = this.items.length;
+    // or: this.length = 0;
   }
 
   add(item) {
-    // use .push() to add element to the array
-    this.items.push(item);
-    // add +1 on the length of the array
-    this.length ++;
-    // sorting array is not necessity but will help us in the later steps
-    this.items.sort((a, b) => a - b);
+    if (item !== undefined) {
+      // use .push() to add element to the array
+      this.items.push(item);
+      // add +1 on the length of the array
+      this.length++;
+      this.items.sort((a, b) => a - b);
+    }
+    return this.items;
   }
 
   get(pos) {
-    // if the array is empty (!this.length) 
+    // if the array is empty (!this.length)
     // or we are trying to get some element on the position that exceeds the number of elements in the array
     // throw an error
-    if(!this.length || pos > this.length){
-      throw new Error("OutOfBounds");
+    if (!this.length || pos > this.length) {
+      throw new Error('OutOfBounds');
     }
 
     // if all is good, return the element in the asked position
@@ -30,12 +32,13 @@ class SortedList {
 
   max() {
     // if the array is empty (!this.length)
-    if(!this.length){
-      // throw an error
-      throw new Error ("EmptySortedList");
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
     }
-    // since we work with sorted array, we can simply point the last element of the array as the highest value of the array
-    return this.items[this.length-1];
+
+    // since we work with sorted array, we can simply point the last element of the array as
+    // the highest value of the array
+    return this.items[this.length - 1];
 
     // you can use some more fancy ways as well
     // return Math.max(...this.items);
@@ -43,31 +46,29 @@ class SortedList {
 
   min() {
     // if the array is empty (!this.length)
-    if(!this.length){
-      // throw an error
-      throw new Error ("EmptySortedList");
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
     }
 
-    // since we work with sorted array, we can simply point the first element of the array as the lowest value of the array
-    return this.items[0];
+    return Math.min(...this.items);
+
+    // or, since we work with sorted array, we can simply point the first element of
+    // the array as the lowest value of the array
+    // return this.items[0];
   }
 
   sum() {
-    // if the array is empty (!this.length)
-    if(!this.length){
-      // sum of the array will be equal to zero
-      return 0;
-    }
-    // using .reduce() array method, we can calculate the sum of the array
-    return this.items.reduce((total, currentValue ) => total + currentValue );
+    // return !this.length ? 0 : this.items.reduce((acc, val) => acc + val);
+    return this.items.reduce((acc, val) => acc + val, 0);
   }
 
-  average() {
-    if(!this.length){
-      throw newError ("EmptySortedList");
+  avg() {
+    // !this.length
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
     }
     // we get average simple as dividing sum with the number of elements in the array (which is the length of the array)
-    return this.sum()/this.length;
+    return this.sum() / this.length;
   }
 }
 
