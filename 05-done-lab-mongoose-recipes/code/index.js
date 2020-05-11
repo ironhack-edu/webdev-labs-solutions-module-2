345b08dc095e1c640dd062a68c911d119194f10e
sandrabosk <sandraboskovic@hotmail.com>
2020-05-11 14:25:38 -0400

    update recipes  and cinema labs

--- 05-done-lab-mongoose-recipes/code/index.js
+++ 05-done-lab-mongoose-recipes/code/index.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const Recipe = require('./models/Recipe.model'); // Import of the model Recipe from './models/Recipe'
+const data = require('./data.js'); // Import of the data from './data.js'
 
-// Connection to the database "recipeApp"
-mongoose
-  .connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
-  .then(() => console.log('Connected to Mongo!'))
-  .catch(err => console.error('Error connecting to mongo', err));
+// require database configuration
+require('./configs/db.config');
 
 // iteration 2
 
@@ -16,14 +13,15 @@ let newRecipe = {
   ingredients: ['pão francês', 'queijo', 'presunto'],
   cuisine: 'Brasileira',
   dishType: 'Snack',
-  image: 'http://culinaria.culturamix.com/blog/wp-content/gallery/misto-quente-3/Misto-Quente-6.jpg',
+  image:
+    'http://culinaria.culturamix.com/blog/wp-content/gallery/misto-quente-3/Misto-Quente-6.jpg',
   duration: 5,
   creator: 'JOC'
 };
 
 Recipe
-// we are passing an object (newRecipe) to the .create() method and 
-// under the hood, this method will insert this object as a new document in the database in the "recipes" collection
+  // we are passing an object (newRecipe) to the .create() method and
+  // under the hood, this method will insert this object as a new document in the database in the "recipes" collection
   .create(newRecipe)
   // .then() resolves if everything worked fine
   .then(result => console.log(`recipe added: ${result.title}`))
@@ -33,8 +31,8 @@ Recipe
 // iteration 3
 
 Recipe
-// here we are passing an array of objects to the ".insertMany()" method that literally does what it says:
-// inserts multiple objects into the database as documents of the "recipes" collection
+  // here we are passing an array of objects to the ".insertMany()" method that literally does what it says:
+  // inserts multiple objects into the database as documents of the "recipes" collection
   .insertMany(data)
   .then(result => {
     result.forEach(item => {
@@ -46,26 +44,30 @@ Recipe
 // iteration 4
 
 Recipe
-// under the hood, ".updateOne()" basically works as ".findOneAndUpdate()" method:
-// finds a certain document in the database based on some criteria (in this case based on the "title"), 
-// and sets some of the properties to the new values and saves it in the database
-  .updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+  // under the hood, ".updateOne()" basically works as ".findOneAndUpdate()" method:
+  // finds a certain document in the database based on some criteria (in this case based on the "title"),
+  // and sets some of the properties to the new values and saves it in the database
+  .updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 })
   .then(() => console.log(`The recipe is updated`))
   .catch(err => console.log(err));
 
 // iteration 5
 
 Recipe
-// under the hood, it works as ".findOneAndDelete" method - find doc based on some criteria (in this case title but could be any other property)
-// and removes it from the DB permanently
-  .deleteOne({title: 'Carrot Cake'})
+  // under the hood, it works as ".findOneAndDelete" method - find doc based on some criteria (in this case title but could be any other property)
+  // and removes it from the DB permanently
+  .deleteOne({ title: 'Carrot Cake' })
   .then(() => console.log(`The recipe is deleted`))
   .catch(err => console.log(err));
 
 // iteration 6
 
 mongoose.connection
-// alternative way to using ".close()" method is using ".disconnect()". The best practice is to use ".close()" but you will see sometimes the alternative way as well
+  // alternative way to using ".close()" method is using ".disconnect()". The best practice is to use ".close()" but you will see sometimes the alternative way as well
   .close()
   .then(() => console.log(`connection closed`))
-  .catch(err => console.log(`an error while closing database connection has occurred: ${err}`));
+  .catch(err =>
+    console.log(
+      `an error while closing database connection has occurred: ${err}`
+    )
+  );
